9f9452c103b14f0cd15b23414a25cdf5ffaa3ecb
clippy <securityclippy@securelyinsecure.com>
2017-11-26 23:13:14 -0800

    terraform

--- terraform/dev/api_gateway/outputs.tf
+++ terraform/dev/api_gateway/outputs.tf
@@ -1,3 +1,7 @@
 output "event_path" {
   value = "${module.api_gateway.event_path}"
 }
+
+output "invoke_url" {
+  value = "${module.api_gateway.invoke_url}"
+}
--- terraform/modules/api_gateway/outputs.tf
+++ terraform/modules/api_gateway/outputs.tf
@@ -1,3 +1,7 @@
 output "event_path" {
   value = "${aws_api_gateway_resource.LambdabotResource.path}"
 }
+
+output "invoke_url" {
+  value = "${aws_api_gateway_deployment.lambdabot.invoke_url}"
+}
