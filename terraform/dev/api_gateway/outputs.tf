61b7c304feb5ddd7b0ad41e470426c72ae960855
clippy <securityclippy@securelyinsecure.com>
2017-11-26 23:46:37 -0800

    terraform + lamda env variable bot name

--- terraform/dev/api_gateway/outputs.tf
+++ terraform/dev/api_gateway/outputs.tf
@@ -1,11 +1,3 @@
-output "event_path" {
-  value = "${module.api_gateway.event_path}"
-}
-
-output "invoke_url" {
-  value = "${module.api_gateway.invoke_url}"
-}
-
 output "event_subscription_url" {
   value = "${module.api_gateway.event_subscription_url}"
 }
