5aed5e5519a62ca6bc67681f4fce4d1bbbd6c7c4
clippy <securityclippy@securelyinsecure.com>
2017-11-26 20:01:08 -0800

    update travis

--- tests/test_lambdabot.py
+++ tests/test_lambdabot.py
@@ -25,21 +25,21 @@ def bot_event():
     return event
 
 def test_botname_required():
-    lb = LambdaBot('testbot')
+    lb = bot()
     assert lb.botname == 'testbot'
 
 def test_handle_resp_false():
-    lb = LambdaBot('testbot')
+    lb = bot()
     resp = '{"ok": false}'
     assert lb._handle_resp(resp) == False
 
 def test_handle_resp_true():
-    lb = LambdaBot('testbot')
+    lb = bot()
     resp = '{"ok": true}'
     assert lb._handle_resp(resp) == True
 
 def test_is_bot_msg_true():
-    lb = LambdaBot('testbot', is_lambda=False, verification_token='test_token')
+    lb = bot()
     with open("tests/test_bot_message.json", "r") as f:
         data = json.load(f)
         data['token'] = 'test_token'
@@ -47,7 +47,7 @@ def test_is_bot_msg_true():
     assert lb.is_bot_msg() == True
 
 def test_is_bot_msg_false():
-    lb = LambdaBot('testbot', is_lambda=False, verification_token='test_token')
+    lb = bot()
     with open("tests/test_user_event.json", "r") as f:
         data = json.load(f)
         data['token'] = 'test_token'
